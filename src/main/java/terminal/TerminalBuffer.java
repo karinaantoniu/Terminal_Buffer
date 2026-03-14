e8aa5c0801c6b974a82d15bce45104d0b85579c1
Karina Antoniu <karina.antoniu@gmail.com>
2026-03-14 18:46:03 +0200

    Added some editing options

--- src/main/java/terminal/TerminalBuffer.java
+++ src/main/java/terminal/TerminalBuffer.java
@@ -64,4 +64,73 @@ public class TerminalBuffer {
         this.currentFg = fg;
         this.currentStyles = EnumSet.copyOf(styles);
     }
+
+    public void isEndOfTerminalColumn() {
+        if (cursorX >= width) {
+            cursorX = 0;
+            cursorY++;
+        }
+    }
+
+    public void isEndOfTerminalRow() {
+        if (cursorY >= height)
+            cursorY = height - 1;
+
+        // must implement the scroll
+    }
+
+    public void writeText(String text) {
+        if (text != null) {
+            for (int i = 0; i < text.length(); i++) {
+                char c = text.charAt(i); // get the character
+
+                // check if we arrived at the end of the terminal
+                isEndOfTerminalColumn();
+                isEndOfTerminalRow();
+
+                // set the char in the matrix
+                screen[cursorX][cursorY].set(c, currentBg, currentFg, currentStyles);
+
+                cursorX++;
+            }
+        }
+    }
+
+    public void fillLine(char c) {
+        // fill the end of the line with the characters
+        if (cursorX != width - 1)
+            for (int i = cursorX; i < width; i++)
+                screen[cursorY][i].set(c, currentBg, currentFg, currentStyles);
+        else
+            for (int i = 0; i < width; i++)
+                screen[cursorY][i].set(c, currentBg, currentFg, currentStyles);
+    }
+
+    public void insertLine(String text, int column, int row) {
+        // when inserting i need to move the characters to the right to make room for the text
+        int size = text.length();
+
+        for (int i = cursorX; i >= row; i--) {
+            for (int j = cursorY; j >= column; j--) {
+
+            }
+        }
+    }
+
+    public void insertEmptyLineAtBottomScreen() {}
+
+    public void clearScreen() {}
+
+    public void clearScreenScrollBack() {}
+
+    public void getCharacterAtPosition() {}
+
+    public void getAttributesAtPosition() {}
+
+    public void getLineAsString() {}
+
+    public void getScreenAsString() {}
+
+    public void getScreenScrollBackAsString() {}
+
 }
